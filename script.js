d8a979a4ead7c38ad16f1272f78449a650edb14f
Prateek <prateekarora5796@gmail.com>
2018-03-06 22:52:48 +0530

    Basic javascript app to read name and age

---
+++ script.js
@@ -0,0 +1,8 @@
+var firstName = prompt("What is your first name??");
+var lastName = prompt("What is your last name??");
+var age = prompt("What is your age??");
+
+
+console.log("Your first name is " + firstName)
+console.log("Your last name is " + lastName)
+console.log("Your age is " + age)
